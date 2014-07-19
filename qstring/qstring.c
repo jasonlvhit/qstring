a8e00b71bd1b8deb319e5f945877f77fb973faf7
Jason Lv <jasonlvhit@gmail.com>
2014-07-19 17:51:12 +0800

    intern and hash

--- qstring/qstring.c
+++ qstring/qstring.c
@@ -1,5 +1,5 @@
 #include "qstring.h"
-#include <cassert>
+#include <assert.h>
 
 /*construct a qstring from const char* (char*) 
  *return a qstring, which equal to the type of char*;
@@ -16,43 +16,146 @@ qstring qstrnew(const char *cstr_){
 	s->type = 0;
 	s->q_hash = qstrhash(s ->cstr);
 	s->ref_count = 1;
+	s->hash_size = -1;
 
 	if (s->len < INTERNING_SIZE)
 		return qstrintern(s);
 	return s->cstr;
 }
+
+/*user level function, same to the strcmp*/
+int qstrcmp(const qstring l, const qstring r){
+	return strcmp(l, r);
+}
+
+/*lower level function, serve the function qstrequal as below*/
+bool _qstrequal(const QString* l, const QString* r){
+	/*all the 2 str are INTERNED*/
+	if (l->type == INTERNED && r->type == INTERNED)
+		return false;
+	/*unequal hash value*/
+	if (l->q_hash != r->q_hash)
+		return false;
+
+	return (strcmp(l->cstr, r->cstr) == 0);
+}
+
+/*return true if qstr l equals to the qstr r*/
+bool qstrequal(const qstring l, const qstring r){
+	if (l == r)
+		return true;
+
+	QString *ls = _getQString(l);
+	QString *rs = _getQString(r);
+	return _qstrequal(ls, rs);
+}
+
 /*interning a string into the string pool
  */
 qstring qstrintern(QString *s){
-	if (pool->count >= POOLING_SIZE) // if pool is full
-		return s->cstr;
+	if (pool->count >= pool->size)
+		expand();
 
-	if (pool->node[s->q_hash]){ //if already in the pool
-		QString* temp = pool->node[s->q_hash];
-		temp->ref_count++;
-		qstrfree(s);
-		return temp->cstr;
+	size_t index = (s->q_hash & pool->size);
+	s->hash_size = pool->size;
+	struct qstr_node * q = (struct qstr_node*)malloc(sizeof(struct qstr_node));
+	q->data = s;
+	//memcpy(q->buffer, s->cstr, (s->len + 1)); //?
+	struct qstr_node *temp = pool ->node[index];
+	if (!temp)
+		(pool->count)++;
+
+	while (temp){
+		temp = temp->next;
+		if (strcmp(temp->data->cstr, s->cstr) == 0){
+			_qstrfree(s);
+			temp->data->ref_count++; //reference count ++
+			return temp->data->cstr;
+		}
 	}
-	//else intern it in 
-	pool->node[s->q_hash] = s;
-	(pool->count)++;
-	s->type = INTERNED;
-	return s->cstr;
+
+	temp = q;
+	q->next = NULL;
+	q->data->type = INTERNED;
+	return q->data->cstr;
 }
 
-void qstrfree(QString *s){
-	assert(s->ref_count == 0);
+/*expend the size for the interning pool*/
+void expend(){
+	if (pool ->size == POOLING_SIZE)
+		return 
+
+	pool->size *= 2;
+	if (pool->size == 0)
+		pool->size = POOLING_START_SIZE;
+}
+
+struct qstr_node* _getInternNode(QString* q){
+	size_t index = q->q_hash & q->hash_size;
+	struct qstr_node* temp = pool->node[index];
+	assert(temp);
+	while (temp){
+		if (strcmp(temp->data->cstr, q->cstr) == 0)
+			return temp;
+	}
+	return NULL;
+}
+
+void _internqstrfree(QString* q){
+	assert(q->type == INTERNED);
+	struct qstr_node* qn = _getInternNode(q);
+	if (--(qn->data->ref_count) == 0){
+		free(qn->data);
+		qn = qn->next; //!!!ÄÚ´æÐ¹Â©
+	}
+}
+
+/*user level function, release a qstr s*/
+void qstrfree(qstring s){
+	QString* q = _getQString(s);
+	if (q->type == INTERNED){/*if interned*/
+		return _internqstrfree(q);
+	}
+	_qstrfree(q);
+}
+
+//lower level function, release the str
+static inline void _qstrfree(QString *s){
 	free(s);
 }
 
-long qstrhash(qstring s){
-	return -1;
+/*SDBM Hash algorithm
+*/
+unsigned int qstrhash(qstring str){
+	unsigned int hash = 0;
+	while (str)
+		hash = (str++) + (hash << 6) + (hash << 16) - hash;
+
+	return (hash & 0x7FFFFFFF);
+}
+
+/*append a char* t to the qstring s, with the length of t*/
+qstring qstrncat(qstring s, const char* t, size_t t_len){
+	;
+}
+
+/*append a char* to the qstr s*/
+qstring qstrcat(qstring s, const char* t){
+	return qstrncat(s, t, strlen(t));
 }
 
 /*return the length of qstring, O(1) tata, haha*/
-size_t qstrlen(const qstring s){
-	QString *q = (QString*)(s - sizeof(QString));
-	return q->len;
+static inline size_t qstrlen(const qstring s){
+	return _getQString(s)->len;
+}
+
+/*return the avail size of current qstring*/
+static inline size_t qstravail(const qstring s){
+	return _getQString(s)->free;
+}
+
+static inline QString * _getQString(const qstring s){
+	return (QString*)(s - sizeof(QString));
 }
 
 int main(){
--- qstring/qstring.h
+++ qstring/qstring.h
@@ -1,8 +1,14 @@
 #ifndef __QSTRING_H
 #define __QSTRING_H
 #include <stdio.h>
+#include <stdarg.h>
+
+#define bool int
+#define false 0
+#define true 1
 
 #define POOLING_SIZE 1024
+#define POOLING_START_SIZE 16
 #define INTERNING_SIZE 32
 #define INTERNED 1
 
@@ -13,23 +19,44 @@ typedef struct {
 	size_t free;
 	size_t type;
 	size_t ref_count;
-	long q_hash;
+	size_t hash_size;
+	unsigned int q_hash;
 	char cstr[];
 }QString;
 
+struct qstr_node{
+	QString *data;
+	QString *next;
+	//char buffer[INTERNING_SIZE];
+};
+
 typedef struct String_Pool{
+	struct qstr_node *node[POOLING_SIZE];
+	size_t size; //2 pow, start from 16
 	size_t count;
-	QString *node[POOLING_SIZE];
-}Pool[1];
+}Pool;
 
-Pool pool;
+Pool *pool;
 
-qstring qstrintern(QString*);
+
+/*user level function*/
 qstring qstrnew(const char *);
-size_t qstrlen(const qstring);
-long qstrhash(const qstring);
-void qstrfree(QString*);
+qstring qstrintern(QString*);
+static inline size_t qstrlen(const qstring);
+static inline size_t qstravail(const qstring);
+unsigned int qstrhash(qstring);
+void qstrfree(qstring);
+qstring qstrncat(const char*);
+bool qstrequal(const QString*, const QString*);
+int qstrcmp(const qstring, const qstring);
+int qstrtrim(const qstring);
+qstring qstrcpy(qstring, qstring);
+qstring qstrncpy(qstring, qstring, size_t);
 
+/*lower level function*/
+void _internqstrfree(QString*);
+static inline void _qstrfree(QString*);
+bool _qstrequal(const qstring, const qstring);
 
 
 #endif
